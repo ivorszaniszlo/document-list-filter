51730276549222c67afdd71d7ac138c730a46f74
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2024-06-08 00:30:15 +0200

    ADD: Added DocBlock comments to config.php and improved documentation

--- config.php
+++ config.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Configuration file for paths.
+ *
+ * @return array
+ */
 return [
     'csv_path' => __DIR__ . '/document_list.csv',
     'test_csv_path' => __DIR__ . '/tests/document_test_list.csv',
