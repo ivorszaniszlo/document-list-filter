6c7a5a78e011940a3141efe41c6194a74e092833
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2024-06-10 14:04:30 +0200

    FIX: Ensures the configuration file exists and is readable

--- src/Configuration.php
+++ src/Configuration.php
@@ -18,11 +18,15 @@ class Configuration
      * Configuration constructor.
      * 
      * Loads the configuration settings from the specified file.
+     * Ensures the configuration file exists and is readable.
      *
      * @param string $configFile The path to the configuration file.
      */
     public function __construct($configFile)
     {
+        if (!file_exists($configFile) || !is_readable($configFile)) {
+            throw new \Exception("Configuration file cannot be loaded: {$configFile}");
+        }
         $this->settings = require $configFile;
     }
 
@@ -30,10 +34,10 @@ class Configuration
      * Get a configuration setting by key.
      *
      * @param string $key The key of the configuration setting.
-     * @return mixed|null The value of the configuration setting or null if not found.
+     * @return mixed The value of the configuration setting or null if the key is not set.
      */
     public function get($key)
     {
-        return $key && isset($this->settings[$key]) ? $this->settings[$key] : null;
+        return isset($this->settings[$key]) ? $this->settings[$key] : null;
     }
 }
